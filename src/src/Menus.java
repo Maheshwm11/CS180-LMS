12e7fd880ccd0251e69175bd4cc1f2514d337506
colby <lanierc@purdue.edu>
2021-11-07 14:41:09 -0500

    Created base class Menus.java

---
+++ src/src/Menus.java
@@ -0,0 +1,13 @@
+public class Menus {
+    private User user;
+    private boolean admin;
+
+    public Menus(User user, boolean admin) {
+        this.user = user;
+        this.admin = admin;
+    }
+
+    public void startMenus() {
+
+    }
+}
