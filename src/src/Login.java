9bc86b47fc4558a8d5933d275868dd3d07ccd60f
tiffanykuang <kuang18@purdue.edu>
2021-11-07 15:42:20 -0500

    added code to allow user to create a new account

--- src/src/Login.java
+++ src/src/Login.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Login {
 
     public static void main(String[] args) {
-
         // arraylists function as follows
         // outermost layer is the posts
         // middle layer is the comments
@@ -21,6 +20,9 @@ public class Login {
 
         boolean loop = true;
         int choice = 0;
+        String type = "";
+        String identification = "";
+        User user;
 
         do {
             System.out.println("Login (1) or Make new Acct (2)");
@@ -40,6 +42,7 @@ public class Login {
 
         // implement a cancel system
         switch (choice) {
+            // logging into account
             case 1:
                 do {
                     System.out.println("Enter your username");
@@ -67,13 +70,42 @@ public class Login {
                         loop = false;
                         System.out.println("Success");
                         if (teacher) {
-                            User user = new User(username);
+                            user = new User(username);
                         } else {
-                            User user = new Admin(username);
+                            user = new Admin(username);
                         }
                     }
                 } while (loop);
                 break;
+
+            // creating a new account
+            case 2:
+                // create username for new account
+                do {
+                    System.out.println("Create your username");
+                    username = s.nextLine();
+                    if (username.contains(";")) {
+                        System.out.println("Username can not contain a semicolon(;). Please enter a new username.");
+                    }
+                } while(!username.contains(";"));
+                // create password for new account
+                System.out.println("Create your password");
+                password = s.nextLine();
+                identification = username + ";" + password + ";";
+                System.out.println("Are you a student or a teacher?");
+                type = s.nextLine();
+                type = type.toLowerCase();
+                switch (type) {
+                    case "student":
+                        identification += "student";
+                        user = new User(username);
+                        break;
+                    case "teacher":
+                        identification += "teacher";
+                        user = new Admin(username);
+                        break;
+                }
+                logins.add(identification);
         }
 
         loop = true;
