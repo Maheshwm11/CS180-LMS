83d73d6a6b60b121b76c8c129f793d651dca7f5d
colby <lanierc@purdue.edu>
2021-11-07 16:03:17 -0500

    Beginning of menus.java

--- src/src/Menus.java
+++ src/src/Menus.java
@@ -1,22 +1,26 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Menus {
     private User user;
+    private ArrayList<Post> discussionPosts;
 
-    public Menus(User user) {
+    public Menus(User user, ArrayList discussionPosts) {
         this.user = user;
+        this.discussionPosts = discussionPosts;
     }
 
     public void startMenus() {
         Scanner s = new Scanner(System.in);
 
-
         boolean loop = true;
         do {
-            System.out.println("pick");
-            System.out.println("view posts");
-            switch (s.nextLine()) {
+            System.out.println("What course would you like to view\ntype all to view all courses");
+            System.out.println("Course list");
 
+            String response = s.nextLine();
+            for (int i = 0; i < discussionPosts.size(); i++) {
+                String course = discussionPosts.get(i).getCourse();
             }
         } while (loop);
     }
