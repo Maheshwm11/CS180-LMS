028327f8101420739aebf06253dbc7076109e24f
colby <lanierc@purdue.edu>
2021-11-09 09:17:38 -0500

    edits to menus

--- src/src/Menus.java
+++ src/src/Menus.java
@@ -12,16 +12,23 @@ public class Menus {
 
     public void startMenus() {
         Scanner s = new Scanner(System.in);
+        ArrayList<String> courses = new ArrayList<>();
 
         boolean loop = true;
         do {
             System.out.println("What course would you like to view\ntype all to view all courses");
             System.out.println("Course list");
 
-            String response = s.nextLine();
             for (int i = 0; i < discussionPosts.size(); i++) {
                 String course = discussionPosts.get(i).getCourse();
+                if (!courses.contains(course)) {
+                    courses.add(course);
+                    System.out.println(course);
+                }
             }
+
+            String response = s.nextLine();
+
         } while (loop);
     }
 }
