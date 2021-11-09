c0ea9e57a7c0292ea960b4b32f73ef2c374291c2
Joseph Seohyun Lee <89218792+lee3296@users.noreply.github.com>
2021-11-09 18:24:46 -0500

    User class test cases
    Finished User class test cases except for last method that hasn't been implemented yet

--- src/src/TestCases.java
+++ src/src/TestCases.java
@@ -6,8 +6,86 @@ public class TestCases {
         /*
         Start of User Class Test Cases
         */
+        
+        User jimBob = new User("jimBob25");
 
+        /**
+         * Beginning of getGrade() and setGrade() test case
+         *
+         * expected: 95
+         *
+         *
+         * Result: 95 (Passes Test Case)
+         *
+         */
+        try {
+            jimBob.setGrade(95); //should set grade to 95
+            if(jimBob.getGrade() != 95) {
+                System.out.println("The grade is not" + jimBob.getGrade());
+            } else {
+                System.out.println("The grade is correct!");
+            }
+        } catch (Exception e) {
+            System.out.println("Make sure that setGrade and getGrade are " +
+                    "implemented correctly!");
+            e.printStackTrace();
+        }
 
+        //TODO: not tested yet
+        /**
+         * Beginning of getUsername() test case
+         *
+         * expected:
+         *
+         *
+         * Result:
+         *
+         */
+        try {
+            if(jimBob.getUsername().equals("jimBob25")) {
+                System.out.println("The username is correct!\n" + jimBob.getUsername());
+            } else {
+                System.out.println("The grade is not" + jimBob.getUsername());
+            }
+        } catch (Exception e) {
+            System.out.println("Make sure that getUsername() is implemented properly!");
+            e.printStackTrace();
+        }
+
+        //TODO: Make sure to test
+        /**
+         * Beginning of replyWithFile() test case
+         *
+         * expected:
+         *
+         *
+         * Result:
+         *
+         */
+        try {
+            Post post = new Post("Project1.txt", "jimBob25", "CS180");
+            jimBob.replyWithFile(post, "meancomment.txt");
+            //should post meancomment.txt to post object
+            
+            System.out.println(post.getComments());
+            //should print out contents of meancomment.txt
+        } catch (Exception e) {
+            System.out.println("Make sure there are no errors in either replyWithFile or " +
+                    "getComments!");
+            e.printStackTrace();
+        }
+
+        //TODO: implement test case when method is completely written
+        /**
+         * Beginning of replyWithString test case
+         *
+         * expected:
+         *
+         *
+         * Result:
+         *
+         */
+        
         /*
         Start of Admin Class Test Cases
         */
