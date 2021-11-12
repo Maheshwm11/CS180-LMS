382bf7f1d7de5eea1a51cb56884221b34fdb1bad
Madhav Maheshwari <maheshwm@purdue.edu>
2021-11-12 02:12:06 -0500

    Added method to rebuild all arrays after restarting

--- src/src/Data.java
+++ src/src/Data.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Data {
 
@@ -10,6 +9,101 @@ public class Data {
     private ArrayList<String> forumName =  new ArrayList<>();
     private ArrayList<String> reply = new ArrayList<>();
     private ArrayList<String> comment = new ArrayList<>();
+
+
+    public void rebuildArrays() {
+        Path path = FileSystems.getDefault().getPath("").toAbsolutePath();
+        ArrayList<String> logins = getLoginFile();
+
+        for (int i = 0; i < logins.size() ; i++) {
+            String dirName = path.toString();
+            String[] details  = logins.get(i).split(";");
+            String userName = details[0];
+            if (details[2].equals("student"))
+                dirName += "/Database/Student/" + userName;
+            else if (details[2].equals("teacher"))
+                dirName += "/Database/Teacher/" + userName;
+            File dir = new File(dirName);
+            if (dir.exists()) {
+                File[] directoryList = dir.listFiles();
+                if (directoryList != null) {
+                    for (File test : directoryList) {
+                        String name = test.getName();
+                        name = name.substring(0, name.length() - 4);
+                        //gets name of file without .txt
+
+                        String[] array = name.split(";");
+                        String content = readFile(name);
+                        switch (array.length) {
+                            case 1: //file is a course title
+                                courseName.add(Integer.parseInt(array[0]), content);
+                                break;
+                            case 2: //file is a forum post
+                                forumName.add(Integer.parseInt(array[1]), content);
+                                break;
+                            case 3: //file is a reply
+                                reply.add(Integer.parseInt(array[2]), content);
+                                break;
+                            case 4:
+                                comment.add(Integer.parseInt(array[3]), content);
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public String createPostFile(int depth, String userDetail, String content) {
+        //userDetail will be of the format userName;password;roleType
+        String courseIndex = "";
+        String forumIndex = "";
+        String replyIndex = "";
+        String commentIndex = "";
+        String identifier = "";
+        //depth will be 1, 2, 3 or 4 (course title, forum post, reply anc comment respectively)
+        // But the title of the file will be numbered (higher the number, the more recent the edit) like 1;1;1;1
+        // means it was the first comment to the first reply to the first forum in the first course (first means the oldest added)
+        switch (depth) {
+            case 1: //initializing a course (title)
+                courseName.add(content);
+                courseIndex = String.valueOf(courseName.size());
+                identifier = courseIndex;
+                break;
+            case 2: //just a forum post
+                forumName.add(content);
+                forumIndex = String.valueOf(forumName.size());
+                identifier = courseIndex + ";" + forumIndex;
+                break;
+            case 3: //reply to a post;
+                reply.add(content);
+                replyIndex = String.valueOf(reply.size());
+                identifier = courseIndex + ";" + forumIndex + ";" + replyIndex;
+                break;
+            case 4: //comment to a reply on a post
+                comment.add(content);
+                commentIndex = String.valueOf(comment.size());
+                identifier = courseIndex + ";" + forumIndex + ";" + replyIndex + ";" + commentIndex;
+                break;
+        }
+        writeFile(userDetail, identifier, content);
+        return identifier;
+    }
+
+    public String readFile(String name) {
+        ArrayList<String> list = new ArrayList<>();
+        File f = new File(name);
+        try (BufferedReader bfr = new BufferedReader(new FileReader(f))) {
+            String line = bfr.readLine();
+            while (line != null) {
+                list.add(line);
+                line = bfr.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return String.join("\n", list);
+    }
 
     public void writeFile(String userDetail, String identifier, String content) {
         Path path = FileSystems.getDefault().getPath("").toAbsolutePath();
@@ -35,43 +129,20 @@ public class Data {
         }
     }
 
-    public String createPostFile(int depth, String userDetail, String content) {
-        //userDetail will be of the format userName;password;roleType
-        String courseIndex = "";
-        String forumIndex = "";
-        String replyIndex = "";
-        String commentIndex = "";
-        //pathName will be c;d;r;c OR c;d;r OR c;d OR c
-        // But the title of the file will be numbered (higher the number, the more recent the edit) like 1;1;1;1
-        // means it was the first comment to the first reply to the first forum in the first course (first means the oldest added)
-        switch (depth) {
-            case 1: //initializing a course (title)
-                courseName.add(content);
-                courseIndex = String.valueOf(courseName.size());
-                break;
-            case 2: //just a forum post
-                forumName.add(content);
-                forumIndex = String.valueOf(forumName.size());
-                break;
-            case 3: //reply to a post;
-                reply.add(content);
-                replyIndex = String.valueOf(reply.size());
-                break;
-            case 4: //comment to a reply on a post
-                comment.add(content);
-                commentIndex = String.valueOf(comment.size());
-                break;
-            default: //improper formatting
-                break;
-        }
-        String identifier = courseIndex + ";" + forumIndex + ";" + replyIndex + ";" + commentIndex;
-        writeFile(userDetail, identifier, content);
-        return identifier;
+    public ArrayList<String> getCourseName() {
+        return courseName;
+    }
+    public ArrayList<String> getForumName() {
+        return forumName;
+    }
+    public ArrayList<String> getReply() {
+        return reply;
+    }
+    public ArrayList<String> getComment() {
+        return comment;
     }
 
-    public void searchFile(String userDetails) {}
-
-    public ArrayList getLoginFile() {
+    public ArrayList<String> getLoginFile() {
         ArrayList<String> logins = new ArrayList<>();
 
         Path path = FileSystems.getDefault().getPath("").toAbsolutePath();
@@ -105,14 +176,4 @@ public class Data {
             e.printStackTrace();
         }
     }
-
-
-    //TODO In first menu, display all no ";" file content (course names)
-    // in second, display all one ";" file content (forum)
-    // in third, display all two ";" file content (replies)
-    // in fourth, display all three ";" file content (comments)
-    // Build a system that can read already present files and rebuild arraylist upon startup
-    // Build a system that can handles multiple users and attribute edits to users to call back on
-
-
 }
