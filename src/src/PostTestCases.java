7d8a2049d5fd46c18c1c8fe354d1c3073cb93cf3
Joseph Seohyun Lee <89218792+lee3296@users.noreply.github.com>
2021-11-14 11:55:51 -0500

    Finished post test cases

--- src/src/PostTestCases.java
+++ src/src/PostTestCases.java
@@ -7,94 +7,105 @@ public class PostTestCases {
         /*
         Start of Post Class Test Cases
         */
-        //Post newer = new Post("NewFile.txt", "Jake", "CS180", "1;1;1");
+        Post newer = new Post("Placer text for example", "Jake", "CS180", "1;1;1");
 
-
-        //TODO: check if testcases pass
         /**
          * Beginning of upVote and DownVote test cases
          *
          * expected:
+         * 2
+         * 0
+         * 1
+         * 2
          *
-         *
-         * Result:
-         *
+         * Result: (Passes test case)
+         * 2
+         * 0
+         * 1
+         * 2
          */
-        // try {
-        // newer.upVote("mynameisBob"); //should create mynameisBob in ArrayList (0)
-        // newer.upVote("mynameisJeff"); //should create mynameisJeff in ArrayList (1)
+        try {
+            newer.upVote("mynameisBob"); //should create mynameisBob in ArrayList (0)
+            newer.upVote("mynameisJeff"); //should create mynameisJeff in ArrayList (1)
 
-        //System.out.println(newer.getUpVotes()); //should print out 2
-        // System.out.println(newer.getDownVotes()); //should print out 0
+            System.out.println(newer.getUpVotes()); //should print out 2
+            System.out.println(newer.getDownVotes()); //should print out 0
 
-        //newer.downVote("mynameisAmazing"); //should create mynameisAmazing in downVote ArrayList
+            newer.downVote("mynameisAmazing"); //should create mynameisAmazing in downVote ArrayList
 
-        // System.out.println(newer.getDownVotes()); //should print out 1
-        //System.out.println(newer.getUpVotes()); //should print out 1
-        //} catch (Exception e) {
-        // System.out.println("There has either been an ArrayListOutOfBounds Exception or a Format Exception!");
-        // e.printStackTrace();
-        //}
+            System.out.println(newer.getDownVotes()); //should print out 1
+            System.out.println(newer.getUpVotes()); //should print out 2
+        } catch (Exception e) {
+            System.out.println("There has either been an ArrayListOutOfBounds Exception or a Format Exception!");
+            e.printStackTrace();
+        }
 
-        //TODO: test that it is correct
         /**
          * Beginning of getComments() and comment() test cases
          *
          * expected:
+         * This is very insightful! Good job!
+         *
+         * you are a big poo poo head.
          *
          *
-         * Result:
+         * Result: (Passes test case)
+         * This is very insightful! Good job!
          *
+         * you are a big poo poo head.
          */
-        //try {
+        try {
 
-        // newer.comment("goodcomment.txt", "Jeff");
-        //newer.comment("meancomment.txt", "Amber");
-        //int counterWithComments = 0;
+            newer.comment("goodcomment.txt", "Jeff");
+            newer.comment("meancomment.txt", "Amber");
+            String[] textFiles = new String[2];
+            textFiles[0] = "goodcomment.txt";
+            textFiles[1] = "meancomment.txt";
+            int counterWithComments = 0;
 
-        //for (Post c : newer.getComments()) {
-        //System.out.println("Post #" + counterWithComments);
-        //File f = new File(c.getFileName());
-        //FileReader fis = new FileReader(f);
-        //BufferedReader reader = new BufferedReader(fis);
+            for (Post c : newer.getComments()) {
+                File f = new File(textFiles[counterWithComments]);
+                FileReader fis = new FileReader(f);
+                BufferedReader reader = new BufferedReader(fis);
+                while (true) {
+                    String input = reader.readLine();
+                    if (input == null) {
+                        break;
+                    } else {
+                        System.out.println(input);
+                    }
+                    System.out.println();
+                }
+                counterWithComments++;
+            }
+        } catch (Exception e) {
+            System.out.println("Stream has most likely been closed!");
+            e.printStackTrace();
+        }
 
-        //while (true) {
-        // String input = reader.readLine();
-        //if (input == null) {
-        // break;
-        //} else {
-        // System.out.println(input);
-
-// }
-// reader.close();
-// System.out.println();
-//}
-//} catch (Exception e) {
-//  e.printStackTrace();
-// }
-
-//TODO: test that it is correct
-/**
- * Beginning of toString() test case
- *
- * expected:
- *      Jake
- *      This should add to the BodyTextTests.txt file
- *      If it doesn't, there has been an issue
- *      UV: 1
- *      DV: 1
- *      Posted: *the live time of when posted, (no set in stone time)*
- *
- *
- * Result:
- *
- */
-// try {
-// System.out.println(newer.toString());
-
-//} catch (Exception e) {
-//   System.out.println("Most likely to .printf(); statement has been used incorrectly");
-//   e.printStackTrace();
-//      }
+        /**
+         * Beginning of toString() test cases
+         *
+         * expected:
+         *
+         * Jake:
+         * Placer text for example
+         * UV: 2 DV: 1
+         * Posted: (no set in stone time)
+         * comments: 2
+         *
+         * Result: (Passes test case)
+         *
+         * Jake:
+         * Placer text for example
+         * UV: 2 DV: 1
+         * posted: 2021-11-14 11:53:48.224
+         * comments: 2
+         */
+        try {
+            System.out.println(newer.toString());
+        } catch (Exception e) {
+            System.out.println("Most likely the .printf(); statement has been used incorrectly");
+        }
     }
 }
