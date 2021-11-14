9af2425177b30820b176749abc7332fa1633842f
Madhav Maheshwari <maheshwm@purdue.edu>
2021-11-13 20:35:55 -0500

    Changed reading and writing methods.

--- src/src/Data.java
+++ src/src/Data.java
@@ -1,161 +1,36 @@
 import java.io.*;
 import java.nio.file.FileSystems;
-import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Data {
-    //Throughout the program, the word "comment" means reply to a reply to a forum post in a course
-    //and just "reply" means reply to a forum post in a course
-    private ArrayList<String> courseName = new ArrayList<>();
-    private ArrayList<String> forumName = new ArrayList<>();
-    private ArrayList<String> reply = new ArrayList<>();
-    private ArrayList<String> comment = new ArrayList<>();
+    private String dirName = FileSystems.getDefault().getPath("").toAbsolutePath() + "/Database";
+    //above block gets current working directory
+    //Used tutorial https://stackoverflow.com/questions/5797208/java-how-do-i-write-a-file-to-a-specified-directory
 
-
-    public void rebuildArrays() {
-        Path path = FileSystems.getDefault().getPath("").toAbsolutePath();
-        ArrayList<String> logins = getLoginFile();
-
-        for (int i = 0; i < logins.size(); i++) {
-            String dirName = path.toString();
-            String[] details = logins.get(i).split(";");
-            String userName = details[0];
-            if (details[2].equals("student"))
-                dirName += "/Database/Student/" + userName;
-            else if (details[2].equals("teacher"))
-                dirName += "/Database/Teacher/" + userName;
-            File dir = new File(dirName);
-            if (dir.exists()) {
-                File[] directoryList = dir.listFiles();
-                if (directoryList != null) {
-                    Arrays.sort(directoryList);
-                    for (File test : directoryList) {
-                        String name = test.getName();
-                        String content = readFile(name, dirName);
-                        name = name.substring(0, name.length() - 4);
-                        //gets name of file without .txt
-
-                        String[] array = name.split(";");
-                        switch (array.length) {
-                            case 1 -> //file is a course title
-                                    courseName.add(Integer.parseInt(array[0]) - 1, content);
-                            case 2 -> //file is a forum post
-                                    forumName.add(Integer.parseInt(array[1]) - 1, content);
-                            case 3 -> //file is a reply
-                                    reply.add(Integer.parseInt(array[2]) - 1, content);
-                            case 4 -> //file is a comment
-                                    comment.add(Integer.parseInt(array[3]) - 1, content);
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    public String createPostFile(String numberedPathname, String userDetail, String content) {
-        //userDetail will be of the format userName;password;roleType
-        String courseIndex;
-        String forumIndex;
-        String replyIndex;
-        String commentIndex;
-        String identifier = "";
-        //numbered pathName will be the path to the content. For example, reply to first forumPost of first course
-        //will be "1;1;r". We don't care about the index of r because the program can automatically find that.
-        // But the title of the file will be numbered (higher the number, the more recent the edit) like 1;1;1;1
-        // means it was the first comment to the first reply to the first forum in the first course (first means the oldest added)
-        String[] numbers = numberedPathname.split(";");
-        switch (numbers.length) {
-            case 1 -> { //initializing a course (title)
-                courseName.add(content);
-                courseIndex = String.valueOf(courseName.size());
-                identifier = courseIndex;
-            }
-            case 2 -> { //just a forum post
-                forumName.add(content);
-                courseIndex = numbers[0];
-                forumIndex = String.valueOf(forumName.size());
-                identifier = courseIndex + ";" + forumIndex;
-            }
-            case 3 -> { //reply to a post;
-                reply.add(content);
-                courseIndex = numbers[0];
-                forumIndex = numbers[1];
-                replyIndex = String.valueOf(reply.size());
-                identifier = courseIndex + ";" + forumIndex + ";" + replyIndex;
-            }
-            case 4 -> { //comment to a reply on a post
-                comment.add(content);
-                courseIndex = numbers[0];
-                forumIndex = numbers[1];
-                replyIndex = numbers[2];
-                commentIndex = String.valueOf(comment.size());
-                identifier = courseIndex + ";" + forumIndex + ";" + replyIndex + ";" + commentIndex;
-            }
-        }
-        writeFile(userDetail, identifier, content);
-        return identifier;
-    }
-
-    public String readFile(String name, String dirName) {
-        File dir = new File(dirName);
-        ArrayList<String> list = new ArrayList<>();
-        File f = new File(dir, name);
-        try (BufferedReader bfr = new BufferedReader(new FileReader(f))) {
-            String line = bfr.readLine();
-            while (line != null) {
-                list.add(line);
-                line = bfr.readLine();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return String.join("\n", list);
-    }
-
-    public void writeFile(String userDetail, String identifier, String content) {
-        Path path = FileSystems.getDefault().getPath("").toAbsolutePath();
-        String dirName = path.toString();
-        //above block gets current working directory
-        String[] user = userDetail.split(";");
-        //gets info from userDetail string
-        String userName = user[0];
-        if (user[2].equals("student"))
-            dirName += "/Database/Student/" + userName;
-        else if (user[2].equals("teacher"))
-            dirName += "/Database/Teacher/" + userName;
-        //Used tutorial https://stackoverflow.com/questions/5797208/java-how-do-i-write-a-file-to-a-specified-directory
+    public void createPostFile(ArrayList<Post> posts) {
         File dir = new File (dirName);
-        if (!dir.exists())
-            dir.mkdir();
-        identifier += ".txt";
-        File f = new File (dir, identifier);
-        try (PrintWriter pw = new PrintWriter(new FileWriter(f))) {
-            pw.write(content);
+        File f = new File (dir, "Posts.txt");
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(f))) {
+            oos.writeObject(posts);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public ArrayList<String> getCourseName() {
-        return courseName;
-    }
-    public ArrayList<String> getForumName() {
-        return forumName;
-    }
-    public ArrayList<String> getReply() {
-        return reply;
-    }
-    public ArrayList<String> getComment() {
-        return comment;
+    public ArrayList<Post> readPostFile() {
+        File dir = new File(dirName);
+        ArrayList<Post> posts = new ArrayList<>();
+        File f = new File(dir, "Posts.txt");
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(f))) {
+            posts = (ArrayList<Post>) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return posts;
     }
 
     public ArrayList<String> getLoginFile() {
         ArrayList<String> logins = new ArrayList<>();
-
-        Path path = FileSystems.getDefault().getPath("").toAbsolutePath();
-        String dirName = path.toString();
-        dirName += "/Database";
         File dir = new File(dirName);
         File f = new File (dir, "Login Details.txt");
 
@@ -173,9 +48,6 @@ public class Data {
 
     public void setLoginFile(String identifier) {
         //identifier here is userName;password;role
-        Path path = FileSystems.getDefault().getPath("").toAbsolutePath();
-        String dirName = path.toString();
-        dirName += "/Database";
         File dir = new File(dirName);
         File f = new File (dir, "Login Details.txt");
 
@@ -186,14 +58,11 @@ public class Data {
             e.printStackTrace();
         }
     }
-    public ArrayList<String> getVoteFile(String numberedPathname, String typeOfVote) {
+    public ArrayList<String> getVoteFile(String identifier, String typeOfVote) {
         //numbered pathName will be the path to the content. For example, reply to first forumPost of first course
         //will be "1;1;1"
         typeOfVote = typeOfVote.toLowerCase();
-        Path path = FileSystems.getDefault().getPath("").toAbsolutePath();
-        String dirName = path.toString();
-        //above block gets current working directory
-        dirName += "/Database/Votes/" + numberedPathname;
+        dirName += "/Votes/" + identifier;
         File dir = new File(dirName);
         if (!dir.exists())
             return null;
@@ -212,14 +81,11 @@ public class Data {
         return list;
     }
 
-    public void setVoteFile( String numberedPathname, String typeOfVote, ArrayList<String> voteFile) {
+    public void setVoteFile(String identifier, String typeOfVote, ArrayList<String> voteFile) {
         //numbered pathName will be the path to the content. For example, reply to first forumPost of first course
         //will be "1;1;1"
         typeOfVote = typeOfVote.toLowerCase();
-        Path path = FileSystems.getDefault().getPath("").toAbsolutePath();
-        String dirName = path.toString();
-        //above block gets current working directory
-        dirName += "/Database/Votes/" + numberedPathname;
+        dirName += "/Votes/" + identifier;
         File dir = new File(dirName);
         if (!dir.exists())
             dir.mkdir();
