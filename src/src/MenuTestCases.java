4ee81a054b515327ed77198b0bb2a0cd23b39ec4
Joseph Seohyun Lee <89218792+lee3296@users.noreply.github.com>
2021-11-14 16:39:05 -0500

    Update MenuTestCases.java

--- src/src/MenuTestCases.java
+++ src/src/MenuTestCases.java
@@ -46,108 +46,37 @@ public class MenuTestCases {
          * (requires user input in terminal to test)
          *
          * expected (1st output - Not Teacher):
+         * Result: same as expected (passes test case)
          *
          * What would you like to do with this post
+         * CS18200:
          * ProfessorB:
          * CS182 stuff
          * UV: 0 DV: 0
-         * posted: 2021-11-14 12:13:00.294
+         * posted: 2021-11-14 16:07:53.445
          * comments: 0
          * 0) exit to all posts
          * 1) view comments
          * 2) leave a comment
          * 2
-         * enter the comment
+         * Enter the comment. Write 'X' on a new line and press 'ENTER' to end
          * Thank you for the help!
+         * X
+         * CS18200:
          * ProfessorB:
          * CS182 stuff
          * UV: 0 DV: 0
-         * posted: 2021-11-14 12:13:00.294
+         * posted: 2021-11-14 16:07:53.445
          * comments: 1
          * 0) exit to all posts
          * 1) view comments
          * 2) leave a comment
          * 1
-         * 0) jimBob25:
-         * Thank you for the help!
-         * UV: 0 DV: 0
-         * posted: 2021-11-14 12:14:34.055
-         * comments: 0
-         *
-         *
-         *
-         * What would you like to do with these replies
-         * 0) back
-         * or enter the number of the reply
-         * 0
-         * ProfessorB:
-         * CS182 stuff
-         * UV: 0 DV: 0
-         * posted: 2021-11-14 12:13:00.294
-         * comments: 1
-         * 0) exit to all posts
-         * 1) view comments
-         * 2) leave a comment
-         * 1
-         * 0) jimBob25:
-         * Thank you for the help!
-         * UV: 0 DV: 0
-         * posted: 2021-11-14 12:14:34.055
-         * comments: 0
-         *
-         *
-         *
-         * What would you like to do with these replies
-         * 0) back
-         * or enter the number of the reply
-         * 1
-         * What would you like to do with this post
+         * 1) CS18200:
          * jimBob25:
          * Thank you for the help!
          * UV: 0 DV: 0
-         * posted: 2021-11-14 12:14:34.055
-         * comments: 0
-         * 0) exit to all posts
-         * 1) view comments
-         * 2) leave a comment
-         * 0
-         * ProfessorB:
-         * CS182 stuff
-         * UV: 0 DV: 0
-         * posted: 2021-11-14 12:13:00.294
-         * comments: 1
-         * 0) exit to all posts
-         * 1) view comments
-         * 2) leave a comment
-         *
-         *
-         * Result: (passes test case)
-         *
-         * What would you like to do with this post
-         * ProfessorB:
-         * CS182 stuff
-         * UV: 0 DV: 0
-         * posted: 2021-11-14 12:13:00.294
-         * comments: 0
-         * 0) exit to all posts
-         * 1) view comments
-         * 2) leave a comment
-         * 2
-         * enter the comment
-         * Thank you for the help!
-         * ProfessorB:
-         * CS182 stuff
-         * UV: 0 DV: 0
-         * posted: 2021-11-14 12:13:00.294
-         * comments: 1
-         * 0) exit to all posts
-         * 1) view comments
-         * 2) leave a comment
-         * 1
-         * 0) jimBob25:
-         * Thank you for the help!
-         * UV: 0 DV: 0
-         * posted: 2021-11-14 12:14:34.055
+         * posted: 2021-11-14 16:08:04.83
          * comments: 0
          *
          *
@@ -156,49 +85,206 @@ public class MenuTestCases {
          * 0) back
          * or enter the number of the reply
          * 0
+         * CS18200:
          * ProfessorB:
          * CS182 stuff
          * UV: 0 DV: 0
-         * posted: 2021-11-14 12:13:00.294
+         * posted: 2021-11-14 16:07:53.445
          * comments: 1
          * 0) exit to all posts
          * 1) view comments
          * 2) leave a comment
          * 1
-         * 0) jimBob25:
-         * Thank you for the help!
-         * UV: 0 DV: 0
-         * posted: 2021-11-14 12:14:34.055
-         * comments: 0
-         *
-         *
-         *
-         * What would you like to do with these replies
-         * 0) back
-         * or enter the number of the reply
-         * 1
-         * What would you like to do with this post
+         * 1) CS18200:
          * jimBob25:
          * Thank you for the help!
          * UV: 0 DV: 0
-         * posted: 2021-11-14 12:14:34.055
+         * posted: 2021-11-14 16:08:04.83
+         * comments: 0
+         *
+         *
+         *
+         * What would you like to do with these replies
+         * 0) back
+         * or enter the number of the reply
+         * 1
+         * What would you like to do with this post
+         * CS18200:
+         * jimBob25:
+         * Thank you for the help!
+         * UV: 0 DV: 0
+         * posted: 2021-11-14 16:08:04.83
          * comments: 0
          * 0) exit to all posts
          * 1) view comments
          * 2) leave a comment
-         * 0
-         * ProfessorB:
-         * CS182 stuff
-         * UV: 0 DV: 0
-         * posted: 2021-11-14 12:13:00.294
-         * comments: 1
-         * 0) exit to all posts
-         * 1) view comments
-         * 2) leave a comment
+         *
          *
          * expected (2nd output - Is a teacher):
          *
-         * Result:
+         * What would you like to do with this post
+         * CS18200:
+         * ProfessorB:
+         * CS182 stuff
+         * UV: 0 DV: 0
+         * posted: 2021-11-14 16:31:58.241
+         * comments: 0
+         * 0) exit to all posts
+         * 1) view comments
+         * 2) leave a comment
+         * 3) edit post
+         * 3
+         * Would you like to use a 1) string or a 2) file
+         * 1
+         * Enter the new body text. Write 'X' on a new line to end
+         * the new edited thing
+         * X
+         * CS18200:
+         * ProfessorB:
+         * the new edited thing
+         * UV: 0 DV: 0
+         * posted: 2021-11-14 16:31:58.241
+         * comments: 0
+         * 0) exit to all posts
+         * 1) view comments
+         * 2) leave a comment
+         * 3) edit post
+         * 3
+         * Would you like to use a 1) string or a 2) file
+         * 2
+         * Enter the path to the file with the content without the name of the file
+         * For example, write 'C:/USER/DATA' if file is in DATA folder
+         * C:\Users\josep\Desktop\mews
+         * Directory found!
+         * Enter the filename with '.txt' suffix. For example, write 'sample.txt'
+         * mews.txt
+         * CS18200:
+         * ProfessorB:
+         * mews
+         * asdfasd
+         * asdf
+         * asdfas
+         * d
+         * 12312
+         * 24324
+         *
+         * $$
+         *
+         * UV: 0 DV: 0
+         * posted: 2021-11-14 16:31:58.241
+         * comments: 0
+         * 0) exit to all posts
+         * 1) view comments
+         * 2) leave a comment
+         * 3) edit post
+         * 2
+         * Enter the comment. Write 'X' on a new line and press 'ENTER' to end
+         * this is my first comment!
+         * X
+         * CS18200:
+         * ProfessorB:
+         * mews
+         * asdfasd
+         * asdf
+         * asdfas
+         * d
+         * 12312
+         * 24324
+         *
+         * $$
+         *
+         * UV: 0 DV: 0
+         * posted: 2021-11-14 16:31:58.241
+         * comments: 1
+         * 0) exit to all posts
+         * 1) view comments
+         * 2) leave a comment
+         * 3) edit post
+         * 1
+         * 1) CS18200:
+         * ProfessorB:
+         * this is my first comment!
+         * UV: 0 DV: 0
+         * posted: 2021-11-14 16:33:04.611
+         * comments: 0
+         *
+         *
+         *
+         * What would you like to do with these replies
+         * 0) back
+         * or enter the number of the reply
+         * 1
+         * What would you like to do with this post
+         * CS18200:
+         * ProfessorB:
+         * this is my first comment!
+         * UV: 0 DV: 0
+         * posted: 2021-11-14 16:33:04.611
+         * comments: 0
+         * 0) exit to all posts
+         * 1) view comments
+         * 2) leave a comment
+         * 3) edit post
+         * 2
+         * Enter the comment. Write 'X' on a new line and press 'ENTER' to end
+         * Second comment
+         * X
+         * CS18200:
+         * ProfessorB:
+         * this is my first comment!
+         * UV: 0 DV: 0
+         * posted: 2021-11-14 16:33:04.611
+         * comments: 1
+         * 0) exit to all posts
+         * 1) view comments
+         * 2) leave a comment
+         * 3) edit post
+         * 1
+         * 1) CS18200:
+         * ProfessorB:
+         * Second comment
+         * UV: 0 DV: 0
+         * posted: 2021-11-14 16:33:23.611
+         * comments: 0
+         *
+         *
+         *
+         * What would you like to do with these replies
+         * 0) back
+         * or enter the number of the reply
+         * 0
+         * CS18200:
+         * ProfessorB:
+         * this is my first comment!
+         * UV: 0 DV: 0
+         * posted: 2021-11-14 16:33:04.611
+         * comments: 1
+         * 0) exit to all posts
+         * 1) view comments
+         * 2) leave a comment
+         * 3) edit post
+         * 0
+         * CS18200:
+         * ProfessorB:
+         * mews
+         * asdfasd
+         * asdf
+         * asdfas
+         * d
+         * 12312
+         * 24324
+         *
+         * $$
+         *
+         * UV: 0 DV: 0
+         * posted: 2021-11-14 16:31:58.241
+         * comments: 1
+         * 0) exit to all posts
+         * 1) view comments
+         * 2) leave a comment
+         * 3) edit post
+         *
+         * Result: same as above (passes test case)
          *
          */
         Post discreteMath = new Post("CS182 stuff", "ProfessorB", "CS18200", "1;2;1");
